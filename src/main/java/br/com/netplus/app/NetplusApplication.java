c140409101eb3cbef92140c157bbd387371b3c1e
Wellington Gomes <welingtong@gmail.com>
2022-12-02 15:06:13 -0300

    cliente, endereco, tipo e carga

--- src/main/java/br/com/netplus/app/NetplusApplication.java
+++ src/main/java/br/com/netplus/app/NetplusApplication.java
@@ -1,13 +1,8 @@
 package br.com.netplus.app;
 
-import br.com.netplus.app.domain.Categoria;
-import br.com.netplus.app.domain.Cidade;
-import br.com.netplus.app.domain.Estado;
-import br.com.netplus.app.domain.Produto;
-import br.com.netplus.app.repositories.CategoriaRepository;
-import br.com.netplus.app.repositories.CidadeRepository;
-import br.com.netplus.app.repositories.EstadoRepository;
-import br.com.netplus.app.repositories.ProdutoRepository;
+import br.com.netplus.app.domain.*;
+import br.com.netplus.app.domain.enums.TipoCliente;
+import br.com.netplus.app.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -33,6 +28,12 @@ public class NetplusApplication implements CommandLineRunner {
 
     @Autowired
     private EstadoRepository estadoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -66,5 +67,23 @@ public class NetplusApplication implements CommandLineRunner {
 
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "81119898154", TipoCliente.PESSOAFISICA );
+        cli1.getTelefones().addAll(Arrays.asList("61987662237", "61987669988"));
+
+        Cliente cli2 = new Cliente(null, "Wellington Gomes LTDA", "wellington@gmail.com", "99988877766", TipoCliente.PESSOAJURIDICA );
+        cli2.getTelefones().addAll(Arrays.asList("61987662237", "61987669988"));
+
+        Endereco e1 = new Endereco(null, "Quadra 21 rua 12", "200", "apt 202", "Taguatinga Norte", "72000000", cli1, c1);
+        Endereco e2 = new Endereco(null, "Quadra 30 rua 1", "100", "cs 12", "Taguatinga Sul", "72000000", cli1, c2);
+
+        Endereco e3 = new Endereco(null, "Rua 11 de cima", "999", "Loja 12", "Guara ", "72000000", cli2, c3);
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli2.getEnderecos().addAll(Arrays.asList(e3));
+
+        clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+        enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
+
     }
 }
