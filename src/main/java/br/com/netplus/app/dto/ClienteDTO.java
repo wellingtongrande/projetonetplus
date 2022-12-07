687ef9d3d4287bcfb02b6bcf65989c8af96532d8
Wellington Gomes <welingtong@gmail.com>
2022-12-07 00:41:30 -0300

    criação de validação de update mantendo e-mail anterior

--- src/main/java/br/com/netplus/app/dto/ClienteDTO.java
+++ src/main/java/br/com/netplus/app/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package br.com.netplus.app.dto;
 
 import br.com.netplus.app.domain.Cliente;
+import br.com.netplus.app.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
